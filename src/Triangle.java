a30baf671645aec582bb04745055daf4ecbec31c
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-14 09:01:09 +0700

    refactor pass test case 2-2-2

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,6 +1,13 @@
 public class Triangle {
    public static String triangleClassification(int a, int b, int c ){
-       String result="Tam giác cân";
+       String result = "";
+       if(a<0|| b<0||c<0){
+           result= "Không phải là tam giác";
+       }else {
+           if(a==b&& a==c){
+               result= "Tam giác đều";
+           }
+       }
        return result;
    }
 }
--- test/TriangleTest.java
+++ test/TriangleTest.java
@@ -10,7 +10,7 @@ class TriangleTest {
         int a=2;
         int b=2;
         int c=2;
-        String expected= "Tam giác cân";
+        String expected= "Tam giác đều";
         String result = Triangle.triangleClassification(a,b,c);
         assertEquals(expected,result);
     }
