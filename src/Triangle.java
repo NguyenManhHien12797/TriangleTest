0564ba102c739f0fbb559ba1b7ee64520b89aa6d
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-14 08:49:49 +0700

    test case 2-2-2

---
+++ src/Triangle.java
@@ -0,0 +1,5 @@
+public class Triangle {
+   public static String triangleClassification(int a, int b, int c ){
+       throw new UnsupportedOperationException();
+   }
+}
---
+++ test/TriangleTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+    @Test
+    @DisplayName("case 2-2-2")
+    public void testa2b2c2(){
+        int a=2;
+        int b=2;
+        int c=2;
+        String expected= "Tam giác cân";
+        String result = Triangle.triangleClassification(a,b,c);
+        assertEquals(expected,result);
+    }
+}
